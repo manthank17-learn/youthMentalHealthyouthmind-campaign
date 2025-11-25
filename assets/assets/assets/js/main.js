66a8e0e3bcb5d47a560fd44036f6a6b5a3ad1877
Manthan Kumar <manthanlearn@gmail.com>
2025-11-25 09:33:42 +0530

    Delete assets/assets/assets/js directory

--- assets/assets/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const yearSpan = document.querySelector("#year");
-    if (yearSpan) {
-        yearSpan.innerText = new Date().getFullYear();
-    }
-});
