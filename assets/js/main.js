274999520651d421123248c42bc24138d1e98438
Manthan Kumar <manthanlearn@gmail.com>
2025-11-25 09:41:24 +0530

    Display current year in year span on page load

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const yearSpan = document.querySelector("#year");
+    if (yearSpan) {
+        yearSpan.innerText = new Date().getFullYear();
+    }
+});
 
