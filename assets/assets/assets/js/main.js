10c49f1baab144d92f0fb23d721593196c53c949
Manthan Kumar <manthanlearn@gmail.com>
2025-11-25 09:30:14 +0530

    Add year display on DOMContentLoaded event

--- assets/assets/assets/js/main.js
+++ assets/assets/assets/js/main.js
@@ -1,1 +1,6 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+    const yearSpan = document.querySelector("#year");
+    if (yearSpan) {
+        yearSpan.innerText = new Date().getFullYear();
+    }
+});
